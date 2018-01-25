560e8781ae453adeb26cd00080c991021ee1546e
Walter Wu <tong.wu@8win.com>
2018-01-25 15:56:26 +0800

    sw precache v0.1

---
+++ next.config.js
@@ -0,0 +1,21 @@
+const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
+// TODO: change into workbox-webpack-plugin
+
+module.exports = {
+  webpack: (config) => {
+    config.plugins.push(
+      new SWPrecacheWebpackPlugin({
+        verbose: true,
+        staticFileGlobsIgnorePatterns: [/\.next\//],
+        runtimeCaching: [
+          {
+            handler: 'networkFirst',
+            urlPattern: /^https?.*/
+          }
+        ]
+      })
+    )
+
+    return config
+  }
+}
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const Koa = require('koa')
 const next = require('next')
 const Router = require('koa-router')
+const serve = require('koa-static');
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
@@ -18,6 +19,7 @@ app.prepare()
       ctx.respond = false
     })
     */
+    server.use(serve(__dirname + '/.next'));
 
     router.get('*', async ctx => {
       await handle(ctx.req, ctx.res)
